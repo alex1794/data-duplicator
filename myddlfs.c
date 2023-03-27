974d05ee5d1e5a6e214130c8bfc84572e40eb775
alex1794 <almaheux@laposte.net>
2023-03-27 15:08:54 +0200

    Add mode argument for reading or writing
    'r' = reading mode
    'w' = writing mode

--- myddlfs.c
+++ myddlfs.c
@@ -11,52 +11,10 @@ double elapsed(struct timespec start, struct timespec stop)
     return (double)stop.tv_sec - (double)start.tv_sec + ( (double)stop.tv_nsec - (double)start.tv_nsec )*1e-9;
 }
 
-int main(int argc, char **argv)
+void write_file(char *filename, unsigned int bs, unsigned int count)
 {
-    srand(getpid());
-
-    int opt;
-	char filename[50];
-	unsigned int bs = 0, count = 0;
-
-	while((opt = getopt(argc, argv, "f:b:c:")) != -1)
-	{
-		switch(opt) {
-			case 'f' :
-				if(sscanf(optarg, "%s", &filename) != 1)
-				{
-					fprintf(stderr, "%s: bad filename", optarg);
-					return 1;
-				}
-				break;
-			case 'b' :
-				if(sscanf(optarg, "%u", &bs) != 1)
-				{
-					fprintf(stderr, "%s: bad block size", optarg);
-					return 1;
-				}
-				break;
-			case 'c':
-				if(sscanf(optarg, "%u", &count) != 1)
-				{
-					fprintf(stderr, "%s: bad number of block", optarg);
-					return 1;
-				}
-				break;
-			default:
-				printf("Usage: %s -f filepath -b blocksize -c number of block\n", argv[0]);
-				exit(EXIT_FAILURE);
-		}
-	}
-
-	if(argc <= 1)
-		return printf("Usage: %s -f filepath -b blocksize -c number of block\n", argv[0]), 1;
-
-	printf("%s - %d - %d\n", filename, bs, count);
-
-
 	struct timespec start, stop;
-    double filesize = (double)bs*count*1e-9;
+    double filesize = (double)bs*count*1e-9; // Gigabyte
 	char *buf = malloc(sizeof(char) * bs);
     for(int i = 0; i < bs; ++i)
         buf[i] = rand() % 100;	
@@ -76,7 +34,7 @@ int main(int argc, char **argv)
 
 	double wrtime = elapsed(start, stop);
     double wrbw = filesize / wrtime;
-	printf("Write of %.2lfG file : %lf secondes, %lf Go/s\n", filesize, wrtime, wrbw);
+	printf("Write of %.2lf GB file : %lf seconds, %lf GB/s\n", filesize, wrtime, wrbw);
 
     free(buf);
 	
@@ -84,6 +42,73 @@ int main(int argc, char **argv)
 	close(fd);
 	clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
 	printf("Close time of %s : %lf secondes\n", filename, elapsed(start, stop));
+}
+
+void read_file(char *filename)
+{
+    printf("Not implemented\n");   
+}    
+
+int main(int argc, char **argv)
+{
+    srand(getpid());
+
+    int opt;
+	char filename[50];
+	unsigned int bs = 0, count = 0;
+    char mode = 0;
+
+	while((opt = getopt(argc, argv, "m:f:b:c:")) != -1)
+	{
+		switch(opt) {
+			case 'm' :
+				if(sscanf(optarg, "%c", &mode) != 1)
+				{
+					fprintf(stderr, "%s: bad mode\n", optarg);
+					return 1;
+				}
+				break;
+			case 'f' :
+				if(sscanf(optarg, "%s", &filename) != 1)
+				{
+					fprintf(stderr, "%s: bad filename\n", optarg);
+					return 1;
+				}
+				break;
+			case 'b' :
+				if(sscanf(optarg, "%u", &bs) != 1)
+				{
+					fprintf(stderr, "%s: bad block size\n", optarg);
+					return 1;
+				}
+				break;
+			case 'c':
+				if(sscanf(optarg, "%u", &count) != 1)
+				{
+					fprintf(stderr, "%s: bad number of block", optarg);
+					return 1;
+				}
+				break;
+			default:
+				fprintf(stderr, "Usage: %s -m mode -f filepath -b blocksize -c number of block\n", argv[0]);
+				exit(EXIT_FAILURE);
+		}
+	}
+
+	if(argc <= 1)
+		return printf("Usage: %s -m mode -f filepath -b blocksize -c number of block\n", argv[0]), 1;
+
+	printf("%c - %s - %d - %d\n", mode, filename, bs, count);
+
+    if(mode == 'w')
+        write_file(filename, bs, count);
+    else if (mode == 'r')
+        read_file(filename);
+    else
+    {
+        fprintf(stderr, "Bad mode : read mode (r) and write mode (w)\n");
+        exit(EXIT_FAILURE);
+    }
 
 	return 0;
 }
