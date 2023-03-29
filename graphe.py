d8a62d5c32a09d0dd7a00c16520704d355b5e9a8
alex1794 <almaheux@laposte.net>
2023-03-29 15:36:16 +0200

    Test boxplot for too big measurement uncertainty
    This is more visual because there is only one or two bad values.

--- graphe.py
+++ graphe.py
@@ -26,8 +26,8 @@ for filename, labels in filelist.items():
         min_array[i] = np.min(temp, axis=0)
         max_array[i] = np.max(temp, axis=0)
 
-    time_yerr = np.concatenate(([mean_array[:,1] - min_array[:,1]], [max_array[:,1] - mean_array[:,1]]), axis=0)
-    bw_yerr   = np.concatenate(([mean_array[:,2] - min_array[:,2]], [max_array[:,2] - mean_array[:,2]]), axis=0)
+    time_yerr = np.concatenate((np.abs([mean_array[:,1] - min_array[:,1]]), np.abs([max_array[:,1] - mean_array[:,1]])), axis=0)
+    bw_yerr   = np.concatenate((np.abs([mean_array[:,2] - min_array[:,2]]), np.abs([max_array[:,2] - mean_array[:,2]])), axis=0)
     
     fig = plt.figure(figsize=(15,12))
     if filename == "bs_c1.txt":
@@ -58,3 +58,12 @@ for filename, labels in filelist.items():
     plt.ylabel("Bandwidth[B/s]")
     plt.title("Writing bandwidth in function of "+labels[1])
     plt.savefig("bw_"+filename[:-4]+".png")
+
+
+fig = plt.figure(figsize=(15,12))
+plt.boxplot(data[:,1:,2].astype(float).transpose(), widths=0.5)
+plt.xticks(np.arange(1, len(mean_array)+1), [str(i[1])+"/"+str(i[2]) for i in head_array], rotation=45)
+plt.xlabel(labels[0])
+plt.ylabel("Bandwidth[B/s]")
+plt.title("Writing bandwidth in function of "+labels[1]+" (bloxplot)")
+plt.savefig("bw_"+filename[:-4]+"_box.png")
