a59acc082e9b8875ec1696b9def577c9e2db465b
alex1794 <almaheux@laposte.net>
2023-03-29 09:39:17 +0200

    Add a measure with constant file size
    Measure time and bandwidth with increasing block size and
    decreasing number of block to keep constant file size to 1GB.

--- bench.sh
+++ bench.sh
@@ -10,18 +10,25 @@ fi
 mntdir=$(echo $1 | sed 's/\/$//')
 
 bsarray=( 500 1000 2000 5000 10000 20000 50000 100000 200000 500000 1000000 2000000 5000000 )
-countarray=( 1 5 10 50 100 500 1000 5000 10000 50000 100000 500000 1000000 5000000 10000000 )
+countarray=( 1 5 10 50 100 500 1000 5000 10000 50000 100000 500000 1000000 2000000 )
+gigarray=( 2000000 1000000 500000 200000 100000 50000 20000 10000 5000 2000 1000 500 200 )
 
+## count=1 and bs=500B to 5M
 for i in "${bsarray[@]}"; do
     ./ddbench -b $i -c 1 $mntdir/benchfile >> bs_c1.txt
     rm $mntdir/benchfile
 done
 
+## count=1 to 2000000 and bs=500B
 for i in "${countarray[@]}"; do
     ./ddbench -b 500 -c $i $mntdir/benchfile >> bs500_c.txt
     rm $mntdir/benchfile
 done
 
+## filesize=1G with count=2000000 to 200 and bs=500B to 5M
+for (( i=0; i<13; i++ )); do
+    ./ddbench -b ${bsarray[$i]} -c ${gigarray[$i]} $mntdir/benchfile >> bs_c_1g.txt
+done
 
 python3 graphe.py
 
