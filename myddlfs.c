78f0f169b8a25c42b5f8be01a137dff413e11b10
alex1794 <almaheux@laposte.net>
2023-03-31 11:10:24 +0200

    Print transfered bytes via InfiniBand
    Read in /sys/class/net/ib0/statistics/ rx_bytes and tx_bytes files
    that give transfered bytes until now via InfiniBand (substrate
    between after and before IO to have transfered bytes of this IO).

--- myddlfs.c
+++ myddlfs.c
@@ -17,6 +17,35 @@ double elapsed(struct timespec start, struct timespec stop)
     return (double)stop.tv_sec - (double)start.tv_sec + ( (double)stop.tv_nsec - (double)start.tv_nsec )*1e-9;
 }
 
+void statib(uint64_t *rx, uint64_t *tx)
+{
+    char buf[15];
+
+    int fdr = open("/sys/class/net/ib0/statistics/rx_bytes", O_RDONLY);
+    if(!fdr)
+    {
+        perror("open fdr");
+        exit(EXIT_FAILURE);
+    }
+
+    int fdt = open("/sys/class/net/ib0/statistics/tx_bytes", O_RDONLY);
+    if(!fdt)
+    {
+        perror("open fdt");
+        exit(EXIT_FAILURE);
+    }
+
+    read(fdr, buf, 15);
+    *rx = atol(buf);
+    read(fdt, buf, 15);
+    *tx = atol(buf);
+
+    close(fdt);
+    close(fdr);
+}
+
+
+
 void write_file(char *filename, uint64_t bs, uint64_t count)
 {
 	struct timespec start;
@@ -24,6 +53,10 @@ void write_file(char *filename, uint64_t bs, uint64_t count)
     struct timespec fsync_start;
     struct timespec fsync_stop;
     double filesize = (double)bs*count;
+    uint64_t rx_start;
+    uint64_t rx_stop;
+    uint64_t tx_start;
+    uint64_t tx_stop;
 #ifndef BENCH
     filesize *= funit[factor]; // for good unit representation
 #endif
@@ -38,6 +71,7 @@ void write_file(char *filename, uint64_t bs, uint64_t count)
 		exit(EXIT_FAILURE);
 	}
 
+    statib(&rx_start, &tx_start);
 	clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 	for(uint64_t i = 0; i < count; ++i)
 		write(fd, buf, bs);
@@ -45,10 +79,12 @@ void write_file(char *filename, uint64_t bs, uint64_t count)
 	fsync(fd);
 	clock_gettime(CLOCK_MONOTONIC_RAW, &fsync_stop);
     clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
+    statib(&rx_stop, &tx_stop);
 
 	double wrtime = elapsed(start, stop);
     double wrbw = filesize / wrtime;
 #ifndef BENCH
+    printf("Transfered bytes via IB : reception=%lu emission=%lu\n", rx_stop-rx_start, tx_stop-tx_start);
     printf("Fsync time : %lf seconds\n", elapsed(fsync_start, fsync_stop));
 	printf("Write of %.2lf %s file : %lf seconds, %lf %s/s\n", filesize, sunit[factor], wrtime, wrbw, sunit[factor]);
 #else
@@ -67,6 +103,10 @@ void read_file(char *filename, uint64_t bs)
 {
 	struct timespec start;
     struct timespec stop;
+    uint64_t rx_start;
+    uint64_t rx_stop;
+    uint64_t tx_start;
+    uint64_t tx_stop;
 	char *buf = malloc(sizeof(char) * bs);
 
 	int fd = open(filename, O_RDONLY);
@@ -77,20 +117,25 @@ void read_file(char *filename, uint64_t bs)
 	}
 
     off_t filesize = lseek(fd, 0, SEEK_END);
+#ifndef BENCH
     printf("Filesize : %lu bytes\n", filesize);
+#endif
     lseek(fd, 0, SEEK_SET);
 
+    statib(&rx_start, &tx_start);
 	clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 	for(off_t i = 0; i < filesize; i+=bs)
         read(fd, buf, bs);
 	fsync(fd);
     clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
+    statib(&rx_stop, &tx_stop);
     //printf("%x\n", buf[0]);
 
 	double rdtime = elapsed(start, stop);
     double rdbw = (double)filesize / rdtime;
 #ifndef BENCH
     rdbw *= funit[factor];
+    printf("Transfered bytes via IB : reception=%lu emission=%lu\n", rx_stop-rx_start, tx_stop-tx_start);
 	printf("Read of %.2lf %s file : %lf seconds, %lf %s/s\n", (double)filesize*funit[factor], sunit[factor], rdtime, rdbw, sunit[factor]);
 #else
     printf("%lu %.8lf %.2lf\n", filesize, rdtime, rdbw);
