51d7f435a28914b23adc6451d837743a2abf8fa1
alex1794 <almaheux@laposte.net>
2023-04-04 11:13:38 +0200

    Add ylim according to device (ram or sda)

--- graph.py
+++ graph.py
@@ -20,7 +20,11 @@ marray = ["read", "write"]
 fig = plt.figure(figsize=(15,12))
 plt.plot(bs[mode==1].astype(str), bw[mode==1]/(1024**2), marker='+', markeredgecolor='r', markersize=12, label=marray[1])
 plt.plot(bs[mode==0].astype(str), bw[mode==0]/(1024**2), marker='+', markeredgecolor='r', markersize=12, label=marray[0])
-plt.yscale("log")
+#plt.yscale("log")
+if filename[6:9] == "ram":
+    plt.ylim(0,2550)
+else:
+    plt.ylim(0,200)
 plt.xlabel("Blocksize [B]")
 plt.ylabel("Bandwidth [MB/s]")
 plt.title("Bandwidth as a function of blocksize ("+filename[6:-4]+")")
