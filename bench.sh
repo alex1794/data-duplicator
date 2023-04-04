959f1d473aa0fdd18f3b7f15ba964c73aca9e714
alex1794 <almaheux@laposte.net>
2023-04-04 11:10:44 +0200

    Add argument for device/filesystem type
    "sda" for device storage only
    "sda_xfs" for device storage in xfs
    "sda_ext4" for device storage in ext4
    "ram" for ram device
    "ram_xfs" for ram device in xfs
    "ram_ext4" for ram device in ext4

--- bench.sh
+++ bench.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo -e "Usage: ./bench.sh <mode(r or w)> <device>"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo -e "Usage: ./bench.sh <mode(r or w)> <fs type> <device>"
     exit
 fi
 
@@ -9,9 +9,8 @@ filesize=$((1*1024*1024*1024))
 
 ## filesize=1G with count=2*1024*1024 to 64 and bs=512B to 16MB
 for (( bs=512; bs<=16*1024*1024; bs=bs*2 )); do
-    echo $bs
-    ./mydd -m $1 -b $bs -c $((filesize/bs)) $2 >> bench_blockdev_$1.txt
+    echo -n "$bs "
+    ./mydd -m $1 -b $bs -c $((filesize/bs)) $3 >> bench_$2_$1.txt
+    echo 3 | sudo tee /proc/sys/vm/drop_caches
 done
-
-python3 graph.py bench_blockdev_$1.txt
 
