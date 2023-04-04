983df881678f11016b7537ffed3ad54f742c2a78
alex1794 <almaheux@laposte.net>
2023-04-04 17:40:47 +0200

    Add flag O_DIRECT in option

--- myddlfs.c
+++ myddlfs.c
@@ -15,7 +15,7 @@ double elapsed(struct timespec start, struct timespec stop)
 	     ((double)stop.tv_nsec - (double)start.tv_nsec) * 1e-9;
 }
 
-void write_file(char *filename, uint64_t bs, uint64_t count)
+void write_file(char *filename, int flags, uint64_t bs, uint64_t count)
 {
 	struct timespec start;
 	struct timespec stop;
@@ -32,7 +32,7 @@ void write_file(char *filename, uint64_t bs, uint64_t count)
 	for (uint64_t i = 0; i < bs; ++i)
 		buf[i] = rand() % 100;
 
-    	int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	int fd = open(filename, flags, S_IRUSR | S_IWUSR);
 	if (fd == -1) {
 		perror("open fd for writing");
 		exit(EXIT_FAILURE);
@@ -54,7 +54,7 @@ void write_file(char *filename, uint64_t bs, uint64_t count)
 	close(fd);
 }
 
-void read_file(char *filename, uint64_t bs, uint64_t count)
+void read_file(char *filename, int flags, uint64_t bs, uint64_t count)
 {
 	struct timespec start;
 	struct timespec stop;
@@ -68,7 +68,7 @@ void read_file(char *filename, uint64_t bs, uint64_t count)
 		exit(EXIT_FAILURE);
 	}
 
-	int fd = open(filename, O_RDONLY);
+	int fd = open(filename, flags);
 	if (fd == -1) {
 		perror("open fd for reading");
 		exit(EXIT_FAILURE);
@@ -77,28 +77,29 @@ void read_file(char *filename, uint64_t bs, uint64_t count)
 	clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 	
 	for (uint64_t i = 0; i < count; i++)
-        	read(fd, buf, bs);
+		read(fd, buf, bs);
 	
 	fsync(fd);
 	clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
 
 	rdtime = elapsed(start, stop);
-    	rdbw = (double)filesize / rdtime;
+	rdbw = (double)filesize / rdtime;
 	printf("%lu %.8lf %.2lf\n", filesize, rdtime, rdbw);
 
-    	free(buf);
+	free(buf);
 	close(fd);
 }    
 
 int main(int argc, char **argv)
 {
-    	int opt;
+	int opt;
 	char filename[50];
 	uint64_t bs = 512;
-    	uint64_t count = 1;
-    	char mode = 'w';
+	uint64_t count = 1;
+	char mode = 'w';
+    int flags = O_WRONLY | O_CREAT | O_TRUNC;
 
-	while ((opt = getopt(argc, argv, "m:b:c:")) != -1) {
+	while ((opt = getopt(argc, argv, "m:b:c:d")) != -1) {
 		switch (opt) {
 		case 'm' :
 			if(sscanf(optarg, "%c", &mode) != 1)
@@ -106,6 +107,10 @@ int main(int argc, char **argv)
 				fprintf(stderr, "%s: bad mode\n", optarg);
 				return 1;
 			}
+
+            if (mode == 'r')
+                flags = O_RDONLY;
+
 			break;
 		case 'b' :
 			if(sscanf(optarg, "%lu", &bs) != 1)
@@ -121,41 +126,43 @@ int main(int argc, char **argv)
 				return 1;
 			}
 			break;
+		case 'd':
+			flags |= O_DIRECT;
+			break;
 		default:
 			fprintf(stderr, usagestr);
 			exit(EXIT_FAILURE);
 		}
 	}
 
-    	argc -= optind;
-    	argv += optind;
-	
-    	if (argc > 0) {
-        	if (argc != 1) {
-		    	return printf(usagestr), 1;
+	argc -= optind;
+	argv += optind;
+
+	if (argc > 0) {
+		if (argc != 1) {
+			return printf(usagestr), 1;
 		}
 
-        	if (sscanf(argv[0], "%s", filename) != 1) {
+		if (sscanf(argv[0], "%s", filename) != 1) {
 			fprintf(stderr, "%s: bad filename\n", argv[0]);
 			return 1;
 		}
-   	} else { 
+	} else {
 		return printf(usagestr), 1;
 	}
 
-	
 	printf("%d %lu %lu ", (mode == 'r') ? 0 : 1, bs, count);
 	
 	srand(getpid());
 
 	if (mode == 'w') {
-        	write_file(filename, bs, count);
+        	write_file(filename, flags, bs, count);
 	} else if (mode == 'r') {
-        	read_file(filename, bs, count);
+        	read_file(filename, flags, bs, count);
 	} else {
         	fprintf(stderr, "Bad mode : read mode (r) and write mode (w)\n");
         	exit(EXIT_FAILURE);
-    	}
+	}
  
 	return 0;
 }
