6b45a58d67e57ca973f764c99705d3dc51e93b27
alex1794 <almaheux@laposte.net>
2023-04-05 13:49:27 +0200

    Change the blocksize range and filesize for O_DIRECT

--- bench.sh
+++ bench.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-    echo -e "Usage: ./bench.sh <mode(r or w or a)> <fs type> <device>"
+    echo -e "Usage: ./bench.sh <mode(r or w or a)> <fs type> <device/file>"
     exit
 fi
 
 filesize=$((1*1024*1024*1024))
+mode=$1
+direct=""
+bsmin=512
+bsmax=$((16*1024*1024))
+if [ "$1" = "wd" ] || [ "$1" = "rd" ]; then
+    echo -n "DIRECT "
+    filesize=$((4*1024*1024*1024))
+    direct="-d"
+    bsmin=4096
+    bsmax=$((2*1024*1024*1024))
+fi
 
 ## filesize=1G with count=2*1024*1024 to 64 and bs=512B to 16MB
 
-if [ "$1" = "w" ] || [ "$1" = "a" ]; then
-    for (( bs=512; bs<=16*1024*1024; bs=bs*2 )); do
+if [ "${mode:0:1}" = "w" ] || [ "${mode:0:1}" = "a" ]; then
+    echo "WRITING"
+    for (( bs=bsmin; bs<=bsmax; bs=bs*2 )); do
         echo -n "$bs "
-        ./mydd -m w -b $bs -c $((filesize/bs)) $3 >> bench_$2_w.txt
-        echo 3 | sudo tee /proc/sys/vm/drop_caches
+        ./mydd -m w -b $bs -c $((filesize/bs)) $direct $3 | tee -a bench_$2_w.txt
+        echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null
     done
 fi
 
-if [ "$1" = "r" ] || [ "$1" = "a" ]; then
-    for (( bs=512; bs<=16*1024*1024; bs=bs*2 )); do
+if [ "${mode:0:1}" = "r" ] || [ "${mode:0:1}" = "a" ]; then
+    echo "READING"
+    for (( bs=bsmin; bs<=bsmax; bs=bs*2 )); do
         echo -n "$bs "
-        ./mydd -m r -b $bs -c $((filesize/bs)) $3 >> bench_$2_r.txt
-        echo 3 | sudo tee /proc/sys/vm/drop_caches
+        ./mydd -m r -b $bs -c $((filesize/bs)) $direct $3 | tee -a bench_$2_r.txt
+        echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null
     done
 fi
 
-if [ "$1" = "a" ]; then
+if [ "${mode:0:1}" = "a" ]; then
     cat bench_$2_w.txt >> bench_$2_wr.txt
     cat bench_$2_r.txt >> bench_$2_wr.txt
     rm bench_$2_w.txt bench_$2_r.txt
